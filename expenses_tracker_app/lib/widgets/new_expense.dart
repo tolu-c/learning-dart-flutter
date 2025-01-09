8540711b0f871d44068706873cf9d7ef993754a3
tolu-c <tolu.adeyemo7@gmail.com>
2025-01-09 01:40:43 +0100

    form validation

--- expenses_tracker_app/lib/widgets/new_expense.dart
+++ expenses_tracker_app/lib/widgets/new_expense.dart
@@ -31,6 +31,33 @@ class _NewExpenseState extends State<NewExpense> {
     });
   }
 
+  void _submitExpenseData() {
+    final enteredAmount = double.tryParse(_amountController.text);
+    final amountIsInValid = enteredAmount == null || enteredAmount <= 0;
+    if (_titleController.text.trim().isEmpty ||
+        amountIsInValid ||
+        _selectedDate == null) {
+      // ! show error message
+      showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: const Text("Invalid Input!"),
+          content: const Text(
+              "Please make sure a valid title, amount, date was entered"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(ctx);
+              },
+              child: const Text("Okay"),
+            )
+          ],
+        ),
+      );
+      return;
+    }
+  }
+
   @override
   void dispose() {
     _titleController.dispose();
@@ -122,10 +149,7 @@ class _NewExpenseState extends State<NewExpense> {
                 child: Text("Cancel"),
               ),
               ElevatedButton(
-                onPressed: () {
-                  print(_titleController.text);
-                  print(_amountController.text);
-                },
+                onPressed: _submitExpenseData,
                 child: const Text("Save"),
               ),
             ],
